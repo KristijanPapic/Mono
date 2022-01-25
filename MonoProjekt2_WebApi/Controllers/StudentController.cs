b0c5d26dd5e8a3c768243c5a316860de5eb2d3fe
Kristijan_Papic <kristijanpapic04@gmail.com>
2022-01-25 15:22:19 +0100

    added projekt2 code and screenshots

---
+++ MonoProjekt2_WebApi/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using MonoProjekt2WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MonoProjekt2WebApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public static List<StudentViewModel> students = new List<StudentViewModel>();
+
+
+        public HttpResponseMessage GetAllStudents ()
+        {
+            if (students.Any()){
+                return Request.CreateResponse(HttpStatusCode.OK, students);
+            }
+            else return Request.CreateResponse(HttpStatusCode.NotFound);
+
+        }
+
+        public HttpResponseMessage GetStudent([FromUri] int id)
+        {
+            if (students.Any(student => student.Id1 == id))
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, students.Where(student => student.Id1 == id));
+            }
+            else return Request.CreateResponse(HttpStatusCode.NotFound);            
+
+        }
+
+        public HttpResponseMessage PostNewStudent([FromBody] StudentViewModel newStudent)
+        {
+            if (students.Any(student => student.Id1 == newStudent.Id1))
+            {
+                return Request.CreateResponse(HttpStatusCode.Conflict, "student with same id already exists");
+            }
+            students.Add(newStudent);
+
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        public HttpResponseMessage Put([FromBody] StudentViewModel updatedStudent)
+        {
+            if(students.Any(student => student.Id1 == updatedStudent.Id1))
+            {
+                int index= students.FindIndex(student => student.Id1 == updatedStudent.Id1);
+                students[index] = updatedStudent;
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+        public HttpResponseMessage Delete([FromUri]int id)
+        {
+            if (students.Any(student => student.Id1 == id))
+            {
+                students.RemoveAll(student => student.Id1 == id);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else return Request.CreateResponse(HttpStatusCode.NotFound);
+            
+        }
+    }
+}
---
+++ MonoProjekt2_WebApi/Models/StudentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MonoProjekt2WebApi.Models
+{
+    public class StudentViewModel
+    {
+        private int Id;
+        private string FirstName;
+        private string LastName;
+        
+        public StudentViewModel(int id,string firstName,string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName1 { get => FirstName; set => FirstName = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+
+        public int Id1 { get => Id; set => Id = value; }
+    }
+}
